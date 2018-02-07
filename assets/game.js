bce81cc56b9591d4f43d35154e03f4c4bce74a12
MininMobile <miningamer@gmail.com>
2018-02-07 17:26:40 +0000

    hunah

--- assets/game.js
+++ assets/game.js
@@ -1,20 +1,22 @@
+function init() {
+
 const cvg = require("convergejs");
 const canvas = document.getElementById("canvas");
 const c = new cvg.Canvas(canvas, "600px", "450px", 60);
 
-function init() {
-	const squares = [
-		// Top Row
-		c.new("box", "tl"),
-		c.new("box", "tm"),
-		c.new("box", "tr"),
-		// Middle Row
-		c.new("box", "ml"),
-		c.new("box", "mm"),
-		c.new("box", "mr"),
-		// Bottom Row
-		c.new("box", "bl"),
-		c.new("box", "bm"),
-		c.new("box", "br")
-	];
+const squares = [
+	// Top Row
+	c.new("box", "tl"),
+	c.new("box", "tm"),
+	c.new("box", "tr"),
+	// Middle Row
+	c.new("box", "ml"),
+	c.new("box", "mm"),
+	c.new("box", "mr"),
+	// Bottom Row
+	c.new("box", "bl"),
+	c.new("box", "bm"),
+	c.new("box", "br")
+];
+
 }
