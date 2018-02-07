4c58bcbef229b6f81f2fd3a01412b967499c9255
MininMobile <miningamer@gmail.com>
2018-02-07 17:21:01 +0000

    fix?

--- assets/game.js
+++ assets/game.js
@@ -3,18 +3,18 @@ const canvas = document.getElementById("canvas");
 const c = new cvg.Canvas(canvas, "600px", "450px", 60);
 
 function init() {
-	const squares = {
+	const squares = [
 		// Top Row
-		"tl":c.new("box", "tl"),
-		"tm":c.new("box", "tm"),
-		"tr":c.new("box", "tr"),
+		c.new("box", "tl"),
+		c.new("box", "tm"),
+		c.new("box", "tr"),
 		// Middle Row
-		"ml":c.new("box", "ml"),
-		"mm":c.new("box", "mm"),
-		"mr":c.new("box", "mr"),
+		c.new("box", "ml"),
+		c.new("box", "mm"),
+		c.new("box", "mr"),
 		// Bottom Row
-		"bl":c.new("box", "bl"),
-		"bm":c.new("box", "bm"),
-		"br":c.new("box", "br")
-	};
+		c.new("box", "bl"),
+		c.new("box", "bm"),
+		c.new("box", "br")
+	];
 }
