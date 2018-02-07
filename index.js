b3f7328e1582a4a214f1fe03a90911a8428fabba
MininMobile <miningamer@gmail.com>
2018-02-07 17:35:13 +0000

    tabboi

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const { app, BrowserWindow } = require('electron');
 const path = require("path"),
-      url = require("url");
+	url = require("url");
 
 let win;
 
 function createWindow() {
-    win = new BrowserWindow();
+	win = new BrowserWindow();
 
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'assets/index.html'),
-        protocol: "file:",
-        slashes: true
-    }));
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'assets/index.html'),
+		protocol: "file:",
+		slashes: true
+	}));
 
-    win.setSize(616, 509);
-    win.focus();
+	win.setSize(616, 509);
+	win.focus();
 
-    win.on("closed", () => {
-        app.quit();
-    });
+	win.on("closed", () => {
+		app.quit();
+	});
 }
 
 app.on("window-all-closed", () => { app.quit(); });
