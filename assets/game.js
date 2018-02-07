e6835c124bd31ce3d95b53691d930b0ed9a294cf
MininMobile <miningamer@gmail.com>
2018-02-07 17:17:46 +0000

    Add Squares

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,20 @@
 const cvg = require("convergejs");
+const canvas = document.getElementById("canvas");
+const c = new cvg.Canvas(canvas, "600px", "450px", 60);
+
+function init() {
+	const squares = {
+		// Top Row
+		"tl":c.new("box", "tl"),
+		"tm":c.new("box", "tm"),
+		"tr":c.new("box", "tr"),
+		// Middle Row
+		"ml":c.new("box", "ml"),
+		"mm":c.new("box", "mm"),
+		"mr":c.new("box", "mr"),
+		// Bottom Row
+		"bl":c.new("box", "bl"),
+		"bm":c.new("box", "bm"),
+		"br":c.new("box", "br")
+	};
+}
