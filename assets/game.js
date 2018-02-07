c97347dd31f0b6109a419a198823684e2b981a1f
MininMobile <miningamer@gmail.com>
2018-02-07 19:52:01 +0000

    overlay

--- assets/game.js
+++ assets/game.js
@@ -19,22 +19,47 @@ const squares = [
 	c.new("box", "br")
 ];
 
+const overlay = [
+	// Top Row
+	c.new("box", "tl-o"),
+	c.new("box", "tm-o"),
+	c.new("box", "tr-o"),
+	// Middle Row
+	c.new("box", "ml-o"),
+	c.new("box", "mm-o"),
+	c.new("box", "mr-o"),
+	// Bottom Row
+	c.new("box", "bl-o"),
+	c.new("box", "bm-o"),
+	c.new("box", "br-o")
+];
+
 squares.forEach(elem => {
 	elem.style([["background", "black"]]);
 	elem.size({ height:"125px", width:"125px" });
 });
 
-squares[1].move({ x:((c.getSize().x/2)-(squares[1].getSize().x/2))+"px" });
-squares[2].move({ x:((c.getSize().x)-(squares[2].getSize().x))+"px" });
-squares[3].move({ y:((c.getSize().y/2)-(squares[3].getSize().y/2))+"px" });
-squares[4].move({ y:((c.getSize().y/2)-(squares[4].getSize().y/2))+"px" });
-squares[4].move({ x:((c.getSize().x/2)-(squares[4].getSize().x/2))+"px" });
-squares[5].move({ y:((c.getSize().y/2)-(squares[5].getSize().y/2))+"px" });
-squares[5].move({ x:((c.getSize().x)-(squares[5].getSize().x))+"px" });
-squares[6].move({ y:((c.getSize().y)-(squares[6].getSize().y))+"px" });
-squares[7].move({ x:((c.getSize().x/2)-(squares[7].getSize().x/2))+"px" });
-squares[7].move({ y:((c.getSize().y)-(squares[7].getSize().y))+"px" });
-squares[8].move({ x:((c.getSize().x)-(squares[8].getSize().x))+"px" });
-squares[8].move({ y:((c.getSize().y)-(squares[8].getSize().y))+"px" });
+overlay.forEach(over => {
+	over.style([["background", "lime"], ["opacity", "0.0"]]);
+	over.size({ height:"125px", width:"125px" });
+});
 
+Spread(squares, c);
+Spread(overlay, c);
+
+}
+
+function Spread(arr, c) {
+	arr[1].move({ x:((c.getSize().x/2)-(arr[1].getSize().x/2))+"px" });
+	arr[2].move({ x:((c.getSize().x)-(arr[2].getSize().x))+"px" });
+	arr[3].move({ y:((c.getSize().y/2)-(arr[3].getSize().y/2))+"px" });
+	arr[4].move({ y:((c.getSize().y/2)-(arr[4].getSize().y/2))+"px" });
+	arr[4].move({ x:((c.getSize().x/2)-(arr[4].getSize().x/2))+"px" });
+	arr[5].move({ y:((c.getSize().y/2)-(arr[5].getSize().y/2))+"px" });
+	arr[5].move({ x:((c.getSize().x)-(arr[5].getSize().x))+"px" });
+	arr[6].move({ y:((c.getSize().y)-(arr[6].getSize().y))+"px" });
+	arr[7].move({ x:((c.getSize().x/2)-(arr[7].getSize().x/2))+"px" });
+	arr[7].move({ y:((c.getSize().y)-(arr[7].getSize().y))+"px" });
+	arr[8].move({ x:((c.getSize().x)-(arr[8].getSize().x))+"px" });
+	arr[8].move({ y:((c.getSize().y)-(arr[8].getSize().y))+"px" });
 }
